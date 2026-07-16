d839597bc844326aa2d3f9de5c538493b5414967
Trần Hải My <my.tran07@hcmut.edu.vn>
2026-07-16 17:36:39 +0700

    style(home): edit dark theme style and hover animation on footer

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { HeroComponent } from '../hero/hero.component';
 import { ContentBoxComponent } from '../content-box/content-box.component';
 import { FooterComponent } from '../footer/footer.component';
@@ -7,7 +8,7 @@ import { ScrollRevealDirective } from '../directives/scroll-reveal.directive';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, HeroComponent, ContentBoxComponent, FooterComponent, ScrollRevealDirective],
+  imports: [CommonModule, RouterLink, HeroComponent, ContentBoxComponent, FooterComponent, ScrollRevealDirective],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
